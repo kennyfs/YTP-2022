2d5861ce0cf06795a76777c431d71c3e345ad657
kenny <kenny.kuo.fs@gmail.com>
2022-12-10 20:48:49 +0800

    製作第一張圖

--- tf-idf-test.py
+++ tf-idf-test.py
@@ -1,31 +1,90 @@
+import itertools
 import json
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import jieba # ldkrsi/jieba-zh_TW for traditional Chinese
+import Graph
+from scipy.spatial import distance
+import networkx as nx
+import matplotlib.pyplot as plt
 data = json.loads(open('cna-category-aipl(example).json','r').read())
-texts = [" ".join(jieba.cut(v['text'], cut_all=False)) for v in data.values()]
-vectorizer = CountVectorizer(token_pattern=r'\b\w+\b')
+texts = [" ".join(jieba.cut(v['text'].lower(), cut_all=False)) for v in data.values()]
+vectorizer = TfidfVectorizer(norm=None)
 vectorizer.fit(texts)
-X = vectorizer.transform(texts)
-
-tfidf_transformer = TfidfTransformer()
-tfidf_transformer.fit(X.toarray())
+tfidf = vectorizer.fit_transform(texts).toarray()
 
 id_to_text = vectorizer.get_feature_names_out()
 print(id_to_text,type(id_to_text),id_to_text.shape)
 
-tfidf = tfidf_transformer.transform(X).toarray()
+print(tfidf.shape)
+word_cnt = {}
+for news in texts:
+    for word in news.split(' '):
+        if word not in word_cnt:
+            word_cnt[word] = 1
+        else:
+            word_cnt[word] += 1
+        #print(word)
 data = []
 for i in range(len(tfidf)):
-    row = [id for id,tfidf_ in enumerate(tfidf[i]) if tfidf_>0.2]
+    row = []
+    for id,tfidf_ in enumerate(tfidf[i]):
+        #print(id,id_to_text[id])
+        #print(word_cnt.get(id_to_text[id],0))
+        #print(id_to_text[id], tfidf_, word_cnt.get(id_to_text[id],0))
+        if tfidf_>10 and word_cnt.get(id_to_text[id],0)>=30:
+            row.append(id_to_text[id])  
+
     data.append(row)
-#處理cooccurrence matrix（未來用networkx的話就是直接連邊）
-cooccurrence = np.zeros((len(id_to_text),len(id_to_text)))
-for row in data:
-    for word in row:
-        for word2 in range(len(row)):
-            if word2 in row and word!=word2:
-                cooccurrence[word,word2] += 1
-#印出一些關鍵字做參考
-for id in data[0]:
-    print(id_to_text[id])
+id_to_text = {}
+text_to_id = {}
+for news in data:
+    for word in news:
+        if word not in text_to_id:
+            text_to_id[word] = len(text_to_id)
+            id_to_text[len(text_to_id)-1] = word
+print(id_to_text)
+
+combinations = [list(itertools.combinations(news, 2)) for news in data]
+combination_matrix = np.zeros((len(id_to_text), len(id_to_text)))
+for tweet_comb in combinations:
+    for comb in tweet_comb:
+        combination_matrix[text_to_id[comb[0]],text_to_id[comb[1]]] += 1
+        combination_matrix[text_to_id[comb[1]],text_to_id[comb[0]]] += 1
+
+for i in range(len(id_to_text)):
+    combination_matrix[i, i] /= 2
+
+jaccard_matrix = 1 - distance.cdist(combination_matrix, combination_matrix, 'jaccard')
+
+nodes = []
+
+for i in range(len(id_to_text)):
+    for j in range(i+1, len(id_to_text)):
+        jaccard = jaccard_matrix[i, j]
+        if jaccard > 0:
+            nodes.append([id_to_text[i], id_to_text[j], word_cnt[id_to_text[i]], word_cnt[id_to_text[j]], jaccard])
+
+G = nx.Graph()
+G.nodes(data=True)
+
+for pair in nodes:
+    node_x, node_y, node_x_cnt, node_y_cnt, jaccard = pair[0], pair[1], pair[2], pair[3], pair[4]
+    if not G.has_node(node_x):
+        G.add_node(node_x, count=node_x_cnt)
+    if not G.has_node(node_y):
+        G.add_node(node_y, count=node_y_cnt)
+    if not G.has_edge(node_x, node_y):
+        G.add_edge(node_x, node_y, weight=jaccard)
+plt.rcParams['font.sans-serif'] = ['AR PL UMing CN']
+plt.figure(figsize=(15,15))
+pos = nx.spring_layout(G, k=0.1)
+
+node_size = [d['count']*10 for (n,d) in G.nodes(data=True)]
+nx.draw_networkx_nodes(G, pos, node_color='cyan', alpha=1.0, node_size=node_size)
+nx.draw_networkx_labels(G, pos, font_family='AR PL UMing CN')
+
+edge_width = [d['weight']*10 for (u,v,d) in G.edges(data=True)]
+nx.draw_networkx_edges(G, pos, alpha=0.2, edge_color='black', width=edge_width)
+
+plt.show()
