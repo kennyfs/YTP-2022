bea164011ef118c0596f5196ef2493a7ed97b28c
AstraytLUL <terrisgd@gmail.com>
2023-03-08 13:37:08 +0800

    Regular Update

--- pythonsrc/YTP_Bot.py
+++ pythonsrc/YTP_Bot.py
@@ -22,8 +22,10 @@ def run(imageFile):
     async def on_message(message):
         if message.author == client.user:
             return
+        
         if message.content == "amogus":
             await message.channel.send("sus")
+            
         if message.content == ">news":
             await message.channel.send("本日新聞統整")
             with open(
@@ -32,5 +34,8 @@ def run(imageFile):
             ) as f:
                 picture = discord.File(f)
                 await message.channel.send(file=picture)
+        
+        if message.content == ">help":
+            await message.channel.send(">news 查看新聞統整圖片\n圖片顏色說明：紅色―出現次數頻繁、藍色—與其他詞大量連接、紫色—同時滿足藍色與紅色")
 
     client.run("TOKEN")
