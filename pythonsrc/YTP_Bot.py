1d6847a18ed5c6831453b7cc542a23d5d5b556b0
AstraytLUL <terrisgd@gmail.com>
2023-03-08 13:46:29 +0800

    Updated News Command

--- pythonsrc/YTP_Bot.py
+++ pythonsrc/YTP_Bot.py
@@ -1,9 +1,10 @@
 import os
 import discord
+import datetime
 from dotenv import load_dotenv
 
 imagePath = "/home/astrayt/YTP2022/"  # default path on someone's computer
-
+todayDate = datetime.date.today().strftime("%Y/%m/%d")
 
 def run(imageFile):
     load_dotenv()
@@ -27,13 +28,12 @@ def run(imageFile):
             await message.channel.send("sus")
             
         if message.content == ">news":
-            await message.channel.send("本日新聞統整")
             with open(
                 imageFile,
                 "rb",
             ) as f:
                 picture = discord.File(f)
-                await message.channel.send(file=picture)
+                await message.channel.send(todayDate + "新聞統整\nSource: 中央社", file=picture)
         
         if message.content == ">help":
             await message.channel.send(">news 查看新聞統整圖片\n圖片顏色說明：紅色―出現次數頻繁、藍色—與其他詞大量連接、紫色—同時滿足藍色與紅色")
