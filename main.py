4cbb6d88a98411a9c4b4d7531af80d0a7159db98
AstraytLUL <terrisgd@gmail.com>
2023-03-09 09:13:06 +0800

    Updated some path.

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if os.path.isfile(imageName) == False :
     yesterday =  today - datetime.timedelta(days=1)
     yesterdayDate = yesterday.strftime("-%m-%d-%Y")
     yesterdayImage = "cna-aipl" + yesterdayDate + ".jpg"
-    if os.path.isfile("/home/Astrayt/YTP-2022/" + yesterdayImage) :
-        os.remove("/home/Astrayt/YTP-2022/" + yesterdayImage)
+    if os.path.isfile(yesterdayImage) :
+        os.remove(AyesterdayImage)
     crawler = CrawlerCNA.CrawlerCNA()
     with CrawlerManager.CrawlerManager(crawler, "cna-aipl.json") as manager:
         manager.getNewsByCategory("政治", 100)
