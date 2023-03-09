3b47bba61449171cbb0bc0145f8b3f940e4314b3
AstraytLUL <terrisgd@gmail.com>
2023-03-09 10:46:03 +0800

    Fixed Bug

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import os
 today = datetime.date.today()
 todayDate = today.strftime("-%m-%d-%Y")
 imageName = "cna-aipl" + todayDate + ".jpg"
-
+JSON_Name = "cna-aipl" + todayDate + ".json"
+ 
 if os.path.isfile(imageName) == False :
     yesterday =  today - datetime.timedelta(days=1)
     yesterdayDate = yesterday.strftime("-%m-%d-%Y")
@@ -14,7 +15,10 @@ if os.path.isfile(imageName) == False :
     if os.path.isfile(yesterdayImage) :
         os.remove(yesterdayImage)
     crawler = CrawlerCNA.CrawlerCNA()
-    with CrawlerManager.CrawlerManager(crawler, "cna-aipl.json") as manager:
-        manager.getNewsByCategory("政治", 100)
-    tfidf.getImage("cna-aipl.json", imageName)
+    if os.path.isfile("cna-aipl" + yesterdayDate + ".json"):
+        os.remove("cna-aipl" + yesterdayDate + ".json")
+    if os.path.isfile(JSON_Name) == False :
+        with CrawlerManager.CrawlerManager(crawler, JSON_Name) as manager:
+            manager.getNewsByCategory("政治", 100)
+    tfidf.getImage(JSON_Name, imageName)
 YTP_Bot.run(imageName)
